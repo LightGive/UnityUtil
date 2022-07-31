9b142db4b1ed067deb62d38a404f4a01afb4519f
LightGive <roi.akase@outlook.jp>
2022-07-31 16:13:53 +0900

    内角が180度以内のひし形の内外判定を追加

---
+++ Runtime/Math/BasicMath.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LightGive.UnityUtil.Math
+{
+	/// <summary>
+	/// 基礎的な計算
+	/// </summary>
+	public class BasicMath
+	{
+        /// <summary>
+        /// ひし形の内外判定
+        /// 内角が180度以下である事が前提
+        /// </summary>
+        /// <returns></returns>
+        public static bool PointInOutForSquareInteriorAngleIn180Dgree(Vector3 checkPoint, Vector3 a, Vector3 b, Vector3 c, Vector3 d)
+        {
+            var ab = Vector3.Cross(b - a, b - checkPoint).z;
+            var bc = Vector3.Cross(c - b, c - checkPoint).z;
+            var cd = Vector3.Cross(d - c, d - checkPoint).z;
+            var da = Vector3.Cross(a - d, a - checkPoint).z;
+            return
+                (ab > 0 && bc > 0 && cd > 0 && da > 0) ||
+                (ab < 0 && bc < 0 && cd < 0 && da < 0);
+        }
+    }
+}
