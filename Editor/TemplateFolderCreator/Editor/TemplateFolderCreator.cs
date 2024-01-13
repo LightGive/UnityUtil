49cf5b4e7f957b54d480b49b8d76a7a0e17ba7a1
LightGive <roi.akase@outlook.jp>
2024-01-14 00:43:24 +0900

    フォルダ整理、プロパティドロワーのUtility追加

---
+++ Editor/TemplateFolderCreator/Editor/TemplateFolderCreator.cs
@@ -0,0 +1,42 @@
+﻿using System.IO;
+using UnityEngine;
+using UnityEditor;
+
+namespace LightGive
+{
+	public class TemplateFolderCreator
+	{
+		[MenuItem("Tools/LightGive/Folder Creator/Create Folder")]
+		static void CreateTemplateFolder()
+		{
+			CreateFolder("GameData");
+			CreateFolder("Scripts");
+			CreateFolder("Prefabs");
+			CreateFolder("Sprites");
+			CreateFolder("Materials");
+			CreateFolder("PhysicsMaterials");
+			CreateFolder("Textures");
+			CreateFolder("Fonts");
+			CreateFolder("Scenes");
+		}
+
+		/// <summary>
+		/// フォルダを作成する
+		/// </summary>
+		/// <returns><c></c>, if folder was created, <c>false</c> otherwise.</returns>
+		/// <param name="_folderName">作成するフォルダ名</param>
+		static bool CreateFolder(string _folderName)
+		{
+			if (Directory.Exists("Assets/" + _folderName))
+			{
+				// フォルダが存在する.
+				Debug.Log("フォルダが存在するため作りませんでした:" + _folderName);
+				return false;
+			}
+
+			string guid = AssetDatabase.CreateFolder("Assets", _folderName);
+			string newFolderPath = AssetDatabase.GUIDToAssetPath(guid);
+			return true;
+		}
+	}
+}
