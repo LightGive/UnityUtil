4d3c3a8906df6bca08432f8a3ed162bd9ed1d4af
LightGive <roi.akase@outlook.jp>
2025-04-09 19:39:17 +0900

    シリアライズ可能なType型を追加

---
+++ Runtime/SerializableType/SerializableType.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System;
+using UnityEngine.Assertions;
+
+namespace LightGive.UnityUtil
+{
+	/// <summary>
+	/// Unityでシリアライズ可能な型情報を保持するクラス
+	/// 型情報を文字列（AssemblyQualifiedName）として保存し、
+	/// シリアライズ後に元の型情報を復元する
+	/// コンストラクタを使ってインスタンスを作る必要があるのでInspectorでは使用不可
+	/// </summary>
+	[Serializable]
+	public class SerializableType : ISerializationCallbackReceiver
+	{
+		[field: SerializeField] public string AssemblyQualifiedName { get; private set; }
+		public Type Type { get; private set; }
+
+		public SerializableType(Type type)
+		{
+			Assert.IsNotNull(type);
+			Type = type;
+			AssemblyQualifiedName = type != null ? type.AssemblyQualifiedName : string.Empty;
+		}
+
+		public override bool Equals(object obj)
+		{
+			if (obj == null)
+			{
+				return false;
+			}
+			var tmp = (SerializableType)obj;
+			if (Type == null || tmp.Type == null)
+			{
+				return Type == null && tmp.Type == null;
+			}
+			return Type.Equals(tmp.Type);
+		}
+
+		public override int GetHashCode()
+		{
+			return Type != null ? HashCode.Combine(Type) : 0;
+		}
+
+		public void OnBeforeSerialize() { }
+
+		public void OnAfterDeserialize()
+		{
+			if (!string.IsNullOrEmpty(AssemblyQualifiedName))
+			{
+				Type = Type.GetType(AssemblyQualifiedName);
+			}
+		}
+
+		public static bool operator ==(SerializableType a, SerializableType b)
+		{
+			if (ReferenceEquals(a, null))
+			{
+				return ReferenceEquals(b, null);
+			}
+			return a.Equals(b);
+		}
+
+		public static bool operator !=(SerializableType a, SerializableType b)
+		{
+			return !(a == b);
+		}
+	}
+}
---
+++ Tests/SerializableTypeTest.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace LightGive.UnityUtil.Tests
+{
+	public class SerializableTypeTest
+	{
+		[NUnit.Framework.Test]
+		public void Equals_ShouldReturnTrue()
+		{
+			{
+				var type1 = new SerializableType(typeof(int));
+				var type2 = new SerializableType(typeof(int));
+				var result = type1.Equals(type2);
+				Assert.IsTrue(result);
+			}
+
+			{
+				var type1 = new SerializableType(typeof(string));
+				var type2 = new SerializableType(typeof(string));
+				var result = type1.Equals(type2);
+				Assert.IsTrue(result);
+			}
+		}
+
+		[NUnit.Framework.Test]
+		public void Equals_ShouldReturnFalse()
+		{
+			{
+				var type1 = new SerializableType(typeof(string));
+				var type2 = new SerializableType(typeof(int));
+				var result = type1.Equals(type2);
+				Assert.IsFalse(result);
+			}
+
+			{
+				var type1 = new SerializableType(typeof(int));
+				var result = type1.Equals(null);
+				Assert.IsFalse(result);
+			}
+
+			{
+				var type1 = new SerializableType(typeof(string));
+				var result = type1.Equals(null);
+				Assert.IsFalse(result);
+			}
+		}
+
+		[NUnit.Framework.Test]
+		public void SerializeTest()
+		{
+			// シリアライズ前と後で同じ値か
+			{
+				// int
+				var t = new SerializableType(typeof(int));
+				var json = JsonUtility.ToJson(t);
+				var deserializedType = JsonUtility.FromJson<SerializableType>(json);
+				Assert.IsTrue(t.Equals(deserializedType));
+			}
+
+			{
+				// string
+				var t = new SerializableType(typeof(string));
+				var json = JsonUtility.ToJson(t);
+				var deserializedType = JsonUtility.FromJson<SerializableType>(json);
+				Assert.IsTrue(t.Equals(deserializedType));
+			}
+		}
+	}
+}
